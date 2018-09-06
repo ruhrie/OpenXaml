f064f3fbdb082508130a3f38ca0dbbc9dafac90e
Richard Uhrie <ruhrie@asu.edu>
2018-09-06 16:08:42 -0700

    most of requirements for glfw hello world

---
+++ OpenXaml/GLFWHelloWorld.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+int main(void)
+{
+    GLFWwindow *window;
+    if (!glfwInit())
+        return -1;
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        //glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
