5c6efd4ebe0f85f7d903e59f5aaa3a54718bb70a
Richard Uhrie <ruhrie@asu.edu>
2018-09-19 12:29:45 -0700

    some xml ex

--- OpenXaml/Parser/Parser.cpp
+++ OpenXaml/Parser/Parser.cpp
@@ -1,12 +1,15 @@
 #include "Parser/Parser.h"
 #include <string>
 #include <fstream>
+#include <xercesc/parsers/XercesDOMParser.hpp>
 
 using namespace std;
+using namespace xercesc;
 namespace OpenXaml {
 	namespace Parser {
 		string ReadFile(string input)
 		{
+			//http://www.yolinux.com/TUTORIALS/XML-Xerces-C.html
 			string fileText;
 			ifstream inputStream;
 			inputStream.open(input);
@@ -16,6 +19,24 @@ namespace OpenXaml {
 				fileText += line;
 			}
 			inputStream.close();
+
+			XMLPlatformUtils::Initialize();
+
+
+			XercesDOMParser* parser = new XercesDOMParser();
+			parser->setValidationScheme(XercesDOMParser::Val_Always);
+			parser->setDoNamespaces(true);    // optional
+
+			
+			parser->parse(input.c_str());
+
+			DOMDocument* xmlDoc = parser->getDocument();
+
+			DOMElement* elementRoot = xmlDoc->getDocumentElement();
+
+
+			XMLPlatformUtils::Terminate();
+
 			return fileText;
 		}
 	}	
