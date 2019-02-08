92b26bf7f80e124f06edeb5805dcf1286b93404d
ruhrie <ruhrie@outlook.com>
2019-02-08 08:45:27 -0700

    Event base definition

---
+++ OpenXaml/include/Events/EventBase.h
@@ -0,0 +1,12 @@
+#include "XamlObjects/XamlObject.h"
+namespace OpenXaml
+{
+    namespace Events
+    {
+        class EventBase
+        {
+        public:
+            XamlObject *Target;
+        };
+    } // namespace Events
+} // namespace OpenXaml
