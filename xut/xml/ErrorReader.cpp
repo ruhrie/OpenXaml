c2c16520bf6fcc27a14f79f978a34aac3b39bf5e
Richard Uhrie <rossette@outlook.com>
2019-08-04 18:51:39 -0700

    reformatted errors

--- xut/xml/ErrorReader.cpp
+++ xut/xml/ErrorReader.cpp
@@ -13,28 +13,25 @@ namespace xut
         {
             XMLFileLoc linex = exc.getLineNumber();
             XMLFileLoc colx = exc.getColumnNumber();
-            cerr << "Warning: XML Parsing warning on line " << linex << ", column " << colx << ".\n";
-            const XMLCh *message = exc.getMessage();
-            string test = XMLString::transcode(message);
-			cerr << test << "\n";
+			const XMLCh* message = exc.getMessage();
+			string test = XMLString::transcode(message);
+            cerr << "Warning: " << test << ", Line: " << linex << ", Column: " << colx << ".\n";
         }
         void ErrorReader::error(const SAXParseException &exc)
         {
             XMLFileLoc linex = exc.getLineNumber();
             XMLFileLoc colx = exc.getColumnNumber();
-            cerr << "Error: XML Parsing exception on line " << linex << ", column " << colx << ".\n";
-            const XMLCh *message = exc.getMessage();
-            string test = XMLString::transcode(message);
-			cerr << test << "\n";
+			const XMLCh* message = exc.getMessage();
+			string test = XMLString::transcode(message);
+			cerr << "Error: " << test << ", Line: " << linex << ", Column: " << colx << ".\n";            
         }
         void ErrorReader::fatalError(const SAXParseException &exc)
         {
             XMLFileLoc linex = exc.getLineNumber();
             XMLFileLoc colx = exc.getColumnNumber();
-            cerr << "Error: XML Parsing exception on line " << linex << ", column " << colx << ".\n";
-            const XMLCh *message = exc.getMessage();
-            string test = XMLString::transcode(message);
-            cerr << test << "\n";
+			const XMLCh* message = exc.getMessage();
+			string test = XMLString::transcode(message);
+			cerr << "Error: " << test << ", Line: " << linex << ", Column: " << colx << ".\n";
         }
         void ErrorReader::resetErrors()
         {
