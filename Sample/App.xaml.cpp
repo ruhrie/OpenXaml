729b835a3e3b23d6726eecbc6532d1ce30d1f841
Richard Uhrie <ruhrie@asu.edu>
2019-08-07 13:48:04 -0700

    dropped unnecessary dependencies

--- Sample/App.xaml.cpp
+++ Sample/App.xaml.cpp
@@ -2,8 +2,6 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
 #include "OpenXaml/XamlObjects/Frame.h"
 #include "OpenXaml/Application.h"
 #include "MainPage.xaml.h"
